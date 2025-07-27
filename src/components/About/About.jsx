87f310cbb76ddd6dd4b5a26df3edd7fcd20346cf
Anurag kumar sahu <72392149+Anurag812000@users.noreply.github.com>
2025-07-27 09:27:46 +0530

    initial commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1q9-LQkPeUebyrQ76mO9kuSDv7eVY2BQn/view?usp=sharing"
+            href="https://drive.google.com/file/d/1dWJ4CKEKpnQWySNvr7-dN0DiQ96B82U8/view?usp=sharinghttps://drive.google.com/file/d/1q9-LQkPeUebyrQ76mO9kuSDv7eVY2BQn/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
